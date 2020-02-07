4d6a4209110c259c8ed1ace9c0696dacc4e9ca0c
Mysh3ll <peacefulloijkn@gmail.com>
2020-02-07 14:07:26 +0100

    Add delete article button

--- src/app/admin/article.service.ts
+++ src/app/admin/article.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Article } from '../models/article';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
+  private readonly baseURL = 'http://localhost:3000/articles';
+  private readonly httpHeaders = {headers: new HttpHeaders({'Content-type': 'application/json'})};
 
   constructor(private readonly httpClient: HttpClient) { }
 
   createArticle(article: Article): Observable<Article> {
-    return this.httpClient.post<Article>('http://localhost:3000/articles', article);
+    return this.httpClient.post<Article>(this.baseURL, article);
+  }
+
+  deleteArticle(article: Article) {
+    const fullURL = `${this.baseURL}/${article._id}`;
+    return this.httpClient.delete<Article>(fullURL, this.httpHeaders);
   }
 }
--- src/app/articles/article-summary/article-summary.component.ts
+++ src/app/articles/article-summary/article-summary.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../models/article';
+import { ArticleService } from "../../admin/article.service";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 @Component({
   selector: 'app-article-summary',
@@ -7,11 +10,42 @@ import { Article } from '../../models/article';
   styleUrls: ['./article-summary.component.scss']
 })
 export class ArticleSummaryComponent implements OnInit {
+  isWaitingForServerResponse: boolean = false;
+  error = null;
   @Input() article: Article;
 
-  constructor() { }
+  constructor(private readonly articleService: ArticleService) { }
 
   ngOnInit() {
   }
 
+  delete(article: Article) {
+    this.isWaitingForServerResponse = true;
+    this.articleService
+      .deleteArticle(article)
+      .pipe(
+        catchError(
+          err => this.handleError(err)
+        )
+      )
+      .subscribe(
+        data => {
+          this.isWaitingForServerResponse = false;
+          this.handleSuccess(data);
+        },
+        err => {
+          this.isWaitingForServerResponse = false;
+          this.handleError(err);
+        },
+      );
+  }
+
+  private handleError(error) {
+    this.error = error;
+    return throwError(this.error);
+  }
+
+  private handleSuccess(data) {
+    console.log('success', data);
+  }
 }
