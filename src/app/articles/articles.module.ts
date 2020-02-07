74003aceebc3b9e18e12b791734e3575fcdcd707
Mysh3ll <peacefulloijkn@gmail.com>
2020-02-07 08:59:39 +0100

    Add get /articles

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { ArticlesComponent } from './articles/articles.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [ArticlesComponent],
   imports: [
     CommonModule,
-    ArticlesRoutingModule
+    ArticlesRoutingModule,
+    HttpClientModule,
   ]
 })
 export class ArticlesModule { }
